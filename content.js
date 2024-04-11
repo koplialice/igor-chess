de576994dab36cdf8643bc78a62bf9f82baa676e
koplialice <chelbossclap@gmail.com>
2024-04-11 12:17:56 +0300

    decimals-ignore

--- content.js
+++ content.js
@@ -1,26 +1,32 @@
-let alertTimesPlayed = [];  
+let alertTimesPlayed = [];
 
 function playAlertSound(soundFile) {
-    const alarmSound = new Audio(chrome.runtime.getURL('audio/' + soundFile));
-    alarmSound.play();
+  const alarmSound = new Audio(chrome.runtime.getURL("audio/" + soundFile));
+  alarmSound.play();
 }
 
 function checkTimer() {
     const timerElement = document.querySelector('.clock-bottom .clock-time-monospace[data-cy="clock-time"]');
     if (timerElement) {
-        const timeText = timerElement.textContent.trim();
-        const [minutes, seconds] = timeText.split(':').map(Number);
+        let timeText = timerElement.textContent.trim();
+
+        let [minutes, seconds] = timeText.split(/[:.]/).map(Number);
         const totalSeconds = minutes * 60 + seconds;
+        console.log(`Adjusted Current Time: ${totalSeconds} seconds`);
 
         chrome.storage.local.get(['alertTimes'], function(result) {
             const alertTimes = result.alertTimes || [];
+            console.log('Alert Times from Storage:', alertTimes);
+
             alertTimes.forEach(({ time, sound }) => {
+                console.log(`Checking Alert Time: ${time} seconds`);
                 if (totalSeconds === time && !alertTimesPlayed.includes(time)) {
+                    console.log(`Playing Sound: ${sound} for Time: ${time} seconds`);
                     playAlertSound(sound);
-                    alertTimesPlayed.push(time); 
+                    alertTimesPlayed.push(time);
                 } else if (totalSeconds !== time && alertTimesPlayed.includes(time)) {
                     const index = alertTimesPlayed.indexOf(time);
-                    alertTimesPlayed.splice(index, 1);  
+                    alertTimesPlayed.splice(index, 1);
                 }
             });
         });
